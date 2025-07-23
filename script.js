1cf1760f21396d0fb76c470be3fb242a008f687e
mayonk <lauriemuga@gmail.com>
2025-07-23 23:19:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+function searchPosts() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const list = document.getElementById("postList").getElementsByTagName("li");
+
+  for (let i = 0; i < list.length; i++) {
+    const post = list[i].textContent.toLowerCase();
+    list[i].style.display = post.includes(input) ? "" : "none";
+  }
+}
